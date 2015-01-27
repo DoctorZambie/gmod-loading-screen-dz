05697d0ee87c197b43a284cebd42e874efdb3dc9
DoctorZambie <DoctorZambie@gmail.com>
2015-01-27 14:35:47 -0800

    Adding files

---
+++ rules.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro">
+
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body> 
+    <div class="container">
+        <div class="jumbotron" style="margin: 50px auto 10px; max-width: 840px;">
+            <img src="img/rule.jpg" alt="" class="pull-right img-rounded" style="width: 240px; height: auto;">
+            <h1 id="title" class="bigEntrance" style="font-size: 50px;">DoctorZambie's Gmod Server :D</h1>
+            <p class="lead">
+                Welcome to my Server. Have fun!<br>
+                <small>
+                    <ul style="line-height: 1.6;">
+                        <li>Follow ALL the rules.</li>
+                        <li>Respect your fellow players/friends .</li>
+                        <li>Don't spam text/Voice chat.
+                        <li>Absolutly NO Discrimination/Racism/etc...!</li>
+                        <li>Absolutly NO Hacking of any sorts.</li>
+                        <li>Follow directions</li>
+                        <li>Listen to admins.</li>
+                    </ul>
+                    All used Workshop items can be found here:
+                    <br>
+                    <code>http://steamcommunity.com/sharedfiles/filedetails/?id=332515460</code><br>→ Servercontent DL (Link)
+                </small>
+            </p>
+
+        </div>
+    </div>
+    <script src="js/vendor/jquery-1.10.1.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
